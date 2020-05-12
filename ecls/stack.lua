1d1750774827d789e17532bc3720d2d984ee9c79
Miqueas Martinez <miqueas2020@yahoo.com>
2020-05-12 02:54:12 -0500

    Comentado el código y modificado el manejo de las pilas

--- ecls/stack.lua
+++ ecls/stack.lua
@@ -3,28 +3,28 @@ require("ecls")
 local unp = table.unpack or unpack
 
 Class("Stack", {
-  main = {},
-  current = "main",
-  stacks = { "main" },
+  _S = { main = {} },  -- Tabla donde se almacenan las pilas
+  current = "main",    -- "Espacio de nombres" en el que se trabaja actualmente
+  stacks = { "main" }, -- Lista de control de pilas
 
-  __init__ = function (self, ...)
+  -- Constructor
+  init = function (self, ...)
     local args = {...}
 
     if #args > 0 then
       for _, v in ipairs(args) do
         if type(v) == "string" then
-          self[v] = {}
+          self._S[v] = {}
           table.insert(self.stacks, v)
         end
       end
     end
-
-    return self
   end,
 
+  -- push: coloca uno o más objetos en la pila
   push = function (self, ...)
     local args = {...}
-    local current = self[self.current]
+    local current = self._S[self.current]
 
     if #args > 0 then
       for _, v in ipairs(args) do
@@ -33,40 +33,58 @@ Class("Stack", {
     end
   end,
 
+  -- pop: retira un objeto en la posición 'pos' o el último en la
+  --      pila si no se especifica 'pos'
   pop = function (self, pos)
-    local current = self[self.current]
+    local current = self._S[self.current]
     table.remove(current, pos)
   end,
 
+  -- get: similar a pop, pero no retira elementos, en su lugar,
+  --      retorna valores
   get = function (self, pos)
-    local current = self[self.current]
+    local current = self._S[self.current]
     local last = #current
     return current[pos] or current[last]
   end,
 
+  -- switch: cambia de espacio de nombres
   switch = function (self, name)
-    assert(self[name], ("Stack '%s' not exists"):format(name))
+    assert(self._S[name], ("Stack '%s' not exists"):format(name))
     self.current = name
   end,
 
+  -- name: retorna el (espacio de) nombre(s) de la pila actual
   name = function (self) return self.current end,
-  len = function (self) return #self[self.current] end,
-  unpack = function (self) return unp(self[self.current]) end,
+  -- len: retorna la cantidad de items en la pila
+  len = function (self) return #self._S[self.current] end,
+  -- unpack: wrapper para "table.unpack" o "unpack" (según la versión de Lua)
+  unpack = function (self) return unp(self._S[self.current]) end,
 
+  -- new: crea (y agrega) una nueva pila
   new = function (self, name)
-    self[name] = {}
+    self._S[name] = {}
     table.insert(self.stacks, name)
   end,
 
+  -- TODO: index: returns the (key or table) index of the
+  --              given argument
   index = function (self, val)
   end,
 
+  -- clear: elimina todos los items dentro de una pila
   clear = function (self, name)
+    assert(self._S[name], ("Stack '%s' not exists"):format(name))
+    self._S[name] = {}
   end,
 
+  -- TODO: destroy: delete an stack from the given name
   destroy = function (self, name)
   end,
 
+  -- list: retorna los nombres de todas las pilas
   list = function (self) return unp(self.stacks) end,
+  -- __len (metamethod): llama al método "len" cuando el operador # es
+  --                     usado
   __len = function (self) return self:len() end
 })
