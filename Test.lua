f9e5a9fc76e9a5310f59f25ac2d7a8b3d6736b2f
Miqueas Martinez <miqueas2020@yahoo.com>
2021-10-20 00:00:30 -0500

    Minor changes in test file

--- Test.lua
+++ Test.lua
@@ -1,5 +1,8 @@
-local Class = require("Self")()
-local Person = Class { }
+local Class, Object = require("Self")(true, true)
+local Person = Class {
+  name = '',
+  age = 0
+}
 
 function Person:new(name, age)
   self.name = name
@@ -7,8 +10,23 @@ function Person:new(name, age)
 end
 
 local a = Person("A", 34)
-local b = Person("B", 53)
+local b = New(Person, "B", 56)
+local c = Object.new(Person, "C", 86)
 print(a.name)
 print(b.name)
+print(c.name)
 print(a.age)
 print(b.age)
+print(c.age)
+
+p(a)
+p(b)
+p(c)
+p(Person)
+
+c.cock = 2
+p(c)
+p(c == c)
+p(a)
+p(b)
+p(Person)
