7d77a0667ebfa6215b10b860a238b5c056808812
Miqueas <miqueas2020@yahoo.com>
2022-01-01 19:30:13 +0000

    Minor changes in test

--- Test.lua
+++ Test.lua
@@ -9,6 +9,10 @@ function Person:new(name, age)
   self.age = age
 end
 
+function Person:greet()
+  print(("Hello, my name is %s and I'm %dyo"):format(self.name, self.age))
+end
+
 local a = Person("A", 34)
 local b = New(Person, "B", 56)
 local c = Object.new(Person, "C", 86)
@@ -21,3 +25,7 @@ p(a)
 p(b)
 p(c)
 p(Person)
+
+a:greet()
+b:greet()
+c:greet()
